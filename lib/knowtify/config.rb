08edf6252b4f1d169666a1c6dd06b90a0db02b5e
Joshua Mckinney <joshmckin@gmail.com>
2015-01-19 16:52:15 -0600

    fix formatting

--- lib/knowtify/config.rb
+++ lib/knowtify/config.rb
@@ -1,25 +1,25 @@
 module Knowtify
   class Config
-    attr_accessor :api_key,	# String  - API key to override ENV['KNOWTIFY_API_TOKEN']
-      :base_url,			      # String  - Base url to build requests from
-      :api_version,			    # String  - API version uses for requests
-      :max_retries,			    # FixNum  - number of times to retry requests
+    attr_accessor :api_key, # String  - API key to override ENV['KNOWTIFY_API_TOKEN']
+      :base_url,            # String  - Base url to build requests from
+      :api_version,         # String  - API version uses for requests
+      :max_retries,         # FixNum  - number of times to retry requests
       :http_client_options, # Hash    - Additional options to pass to the HTTP client
       :debug,               # Boolean - Default is false
       :handler              # Symbol  - Default is :excon
 
 
-      HANDLERS = {
-        :excon => Knowtify::ExconHandler
-      }.freeze
+    HANDLERS = {
+      :excon => Knowtify::ExconHandler
+    }.freeze
 
     def initialize
-      @api_key 				      = ENV['KNOWTIFY_API_TOKEN']
-      @base_url 			      = "http://www.knowtify.io"
-      @api_version 			    = "v1"
-      @http_client_options 	= {}
-      @max_retries 			    = 2
-      @debug 				        = false
+      @api_key              = ENV['KNOWTIFY_API_TOKEN']
+      @base_url             = "http://www.knowtify.io"
+      @api_version          = "v1"
+      @http_client_options  = {}
+      @max_retries          = 2
+      @debug                = false
       @handler              = :excon
     end
 
